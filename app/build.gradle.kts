48a8db41574d7f8f34328947eac442f5e8eab546
mitulbharatchandra <iammitulsheth@gmail.com>
2024-06-19 23:05:49 +0530

    1_2 build-logic convention Adding Library Into version catalog and updated kotlin version and libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packaging {
         resources {
