c0e3c37ceba626ee2194fd943067fa894d4741a9
mitulbharatchandra <iammitulsheth@gmail.com>
2024-06-28 22:33:15 +0530

    2_1 Reading JSON File From the Assets and Kotlinx Serialization

--- core/resource_assets/src/main/kotlin/com/app/multimodule/core/resource_assets/JvmUnitTestFakeAssetManager.kt
+++ core/resource_assets/src/main/kotlin/com/app/multimodule/core/resource_assets/JvmUnitTestFakeAssetManager.kt
@@ -5,11 +5,6 @@ import java.io.File
 import java.io.InputStream
 import java.util.Properties
 
-/**
- * This class helps with loading Android `/assets` files, especially when running JVM unit tests.
- * It must remain on the root package for an easier [Class.getResource] with relative paths.
- * @see <a href="https://developer.android.com/reference/tools/gradle-api/7.3/com/android/build/api/dsl/UnitTestOptions">UnitTestOptions</a>
- */
 @VisibleForTesting
 internal object JvmUnitTestFakeAssetManager : AssetManager {
     override fun open(fileName: String): InputStream = File("../../core/resource_assets/src/main/assets/", fileName).inputStream()
