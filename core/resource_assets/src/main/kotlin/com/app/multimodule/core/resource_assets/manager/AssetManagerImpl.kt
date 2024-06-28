a16b93a8eae8b50809d6d4b550530d3153d6057b
mitulbharatchandra <iammitulsheth@gmail.com>
2024-06-28 22:36:43 +0530

    2_1 Reading JSON File From the Assets and Kotlinx Serialization

--- core/resource_assets/src/main/kotlin/com/app/multimodule/core/resource_assets/manager/AssetManagerImpl.kt
+++ core/resource_assets/src/main/kotlin/com/app/multimodule/core/resource_assets/manager/AssetManagerImpl.kt
@@ -3,7 +3,6 @@ package com.app.multimodule.core.resource_assets.manager
 import android.content.Context
 import com.app.multimodule.core.resource_assets.AssetManager
 import java.io.InputStream
-import javax.inject.Inject
 
 class AssetsManagerImpl(private val context: Context) : AssetManager {
 
