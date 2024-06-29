abb1c28c0fdfa7e0b2d4dd9459d791f0df9b035d
mitulbharatchandra <iammitulsheth@gmail.com>
2024-06-29 14:55:12 +0530

    2_2 Minor changes

--- core/data/src/main/kotlin/com/app/multimodule/core/data/repository/MediaRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/app/multimodule/core/data/repository/MediaRepositoryImpl.kt
@@ -1,12 +1,14 @@
 package com.app.multimodule.core.data.repository
 
-import com.app.multimodule.core.resource_assets.AssetsDataSource
+import com.app.multimodule.core.resource_assets.MediaDataSource
 import com.app.multimodule.core.resource_assets.model.Media
 import javax.inject.Inject
 
-class MediaRepositoryImpl @Inject constructor(private val assetsDataSource: AssetsDataSource) : MediaRepository {
+class MediaRepositoryImpl @Inject constructor(
+    private val mediaDataSource: MediaDataSource
+) : MediaRepository {
 
     override suspend fun getMedia(): Media {
-        return assetsDataSource.getMedia("")
+        return mediaDataSource.getMedia()
     }
 }
